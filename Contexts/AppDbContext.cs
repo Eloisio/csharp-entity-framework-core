2dad57fe2d17b7bb18c65b4a4bb1c0388f3ae87e
Cleyson Lima <cleysonph@gmail.com>
2022-04-13 19:32:16 -0300

    03:04 - mapeando nossa primeira entidade

--- Contexts/AppDbContext.cs
+++ Contexts/AppDbContext.cs
@@ -1,11 +1,14 @@
+using EFCoreExample.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace EFCoreExample.Contexts;
 
 public class AppDbContext : DbContext
 {
+    public DbSet<Cliente> Clientes => Set<Cliente>();
+
     protected override void OnConfiguring(DbContextOptionsBuilder options)
     {
-        options.UseSqlServer("Server=Localhost;Database=EFCoreExample;Trusted_Connection=True;");
+        options.UseSqlServer("Server=Localhost;Database=CursoEFCore;Trusted_Connection=True;");
     }
 }
