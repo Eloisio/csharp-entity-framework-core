52da2927a02fe677efe79290e8704dea0d1929a9
Cleyson Lima <cleysonph@gmail.com>
2022-04-13 20:49:13 -0300

    04:10 - realizando exclusão

--- Program.cs
+++ Program.cs
@@ -1,30 +1,14 @@
 ﻿using EFCoreExample.Contexts;
 using EFCoreExample.Models;
-using Microsoft.EntityFrameworkCore;
 
 var db = new AppDbContext();
 
-// Utilizando registro monitorados
+// Removendo registro rastreado
 var cliente = db.Clientes.First();
-cliente.Nome = "Novo Nome";
+db.Remove(cliente);
 db.SaveChanges();
 
-// Uitlizando Update
-var cliente2 = new Cliente
-{
-    Id = 2,
-    Nome = "Novo Nome",
-    Cpf = "Novo CPF"
-};
-db.Update(cliente2);
-db.SaveChanges();
-
-// Utilizando Entry
-var cliente3 = new Cliente
-{
-    Id = 3,
-    Nome = "Novo Nome",
-    Cpf = "Novo CPF"
-};
-db.Entry(cliente3).State = EntityState.Modified;
+// Removendo por ID
+var cliente2 = new Cliente { Id = 2 };
+db.Remove(cliente2);
 db.SaveChanges();
