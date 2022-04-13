254ac01ccbe5b6c2964cd5a61e48f5e038e3cb74
Cleyson Lima <cleysonph@gmail.com>
2022-04-13 20:12:33 -0300

    04:07 - realizando inserção

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EFCoreExample.Contexts;
+using EFCoreExample.Models;
+
+var db = new AppDbContext();
+
+var cliente1 = new Cliente
+{
+    Nome = "Elton Fonseca",
+    Cpf = "123.456.789-12"
+};
+var cliente2 = new Cliente
+{
+    Nome = "Wesley Gado",
+    Cpf = "456.789.123-34"
+};
+
+db.AddRange(cliente1, cliente2);
+db.SaveChanges();
